ee4098b93046909de12b1fcfac982d77b2cee1a1
Mearnest <mark@treesforlife.org>
2014-01-19 15:56:55 -0600

    init
    Initial release

---
+++ README.md
@@ -0,0 +1,2 @@
+# presentation
+### a Sails application
---
+++ assets/js/navigation.js
@@ -0,0 +1,88 @@
+$(document).ready(function() {
+	function assignAjaxLinks() {
+		$("a.ajax").click(function(event) {
+			event.preventDefault();
+			
+			var page = $(this).attr("href");
+			var title = $(this).attr("data-title");
+			var navigation = $(this).attr("data-nav");
+			var objState = { "title": title, "navigation": navigation }; 
+			
+			$("a.ajax").removeClass("active");
+			$(this).addClass("active");
+			
+			window.history.pushState(objState, title, page);
+			ajaxCall(page, title, navigation);
+		});
+		
+	}
+	
+	function ajaxCall(page, title, navigation) {
+		if (navigation) {
+			$("#sidebar").fadeOut("fast", function() {
+				$.get(navigation + "/ajax", function(data) {
+					bflResizeNavigation();
+					
+					$("#sidebar").html(data);
+					$("a.ajax").removeClass("active");
+					$("a.ajax[data-title='" + title + "']").addClass("active");
+					
+					// Need to assign click handlers for new content.
+					assignAjaxLinks();
+					
+					$("#sidebar").fadeIn("fast", function() {
+						// fade in
+					});
+				});
+			});
+		}
+		
+		$("#main .wrapper").fadeOut("fast", function() {
+			$.get(page + "/ajax", function(data) {
+				$("#main .wrapper").html(data);
+				
+				$("#main .wrapper").fadeIn("fast", function() {
+					// fade in
+				});
+			});
+		});
+	}
+	
+	assignAjaxLinks();
+	
+	// An event that fires when the user hits the browser back, forward or refresh.
+	// This works in conjunction with window.history.pushState.
+	window.onpopstate = function(event) {
+		if (event.state) {
+			// console.log("location: " + document.location + ", state: " + JSON.stringify(event.state));
+			ajaxCall(document.location, event.state.title);
+		}
+	}
+});
+
+
+// Resize the sidebar to contain a navigation.
+// The home page has a 10 column main content are with a 1 col offset.
+// It's navigation is the main content.
+function bflResizeNavigation() {
+	$("#main").removeClass("col-md-10");
+	$("#main").removeClass("col-md-offset-1");
+	$("#main").addClass("col-md-8");
+	
+	$("#sidebar").removeClass("col-md-1");
+	$("#sidebar").addClass("col-md-offset-1");
+	$("#sidebar").addClass("col-md-3");
+}
+
+
+// Use arrow keys to go forward and backward - maybe only for the manual navigation.
+$(document).keydown(function(e) {
+    if (e.keyCode == 37) { 
+       history.back();
+       return false;
+    } 
+	else if(e.keyCode == 39) {
+		history.forward();
+		return false;
+	}
+});
